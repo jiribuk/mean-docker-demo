9b923b3de1d846b24970b0d04795e65e8e53cc5c
jiri <george.buk@gmail.com>
2017-11-12 23:36:28 -0500

    Env vars

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,13 @@
 // api.js
-console.log('api starting');
+console.log('api starting')
 
-const express = require('express');
-const router = express.Router();
-
+const express = require('express')
+const router = express.Router()
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-    res.send('Express API works');
-});
+  const NODE_ENV = process.env.NODE_ENV
+  res.send('Express API works. NODE_ENV=' + NODE_ENV)
+})
 
-
-
-module.exports = router;
+module.exports = router
