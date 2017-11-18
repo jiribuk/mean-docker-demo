e3ff48c3569735b9d423a2c218c8caf6d9a0c7a8
jiri <george.buk@gmail.com>
2017-11-18 14:07:37 -0500

    Formatting text output

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,9 @@ router.get('/', (req, res) => {
     if (err) {
       res.send(err).status(500)
     } else {
-      res.send('API works.<h1>DNS</h1>' + JSON.stringify(address) + '</br>' + JSON.stringify(family) + '<h1>ENV</h1>' + JSON.stringify(process.env).replace(',', '<br/>'))
+      let envVars = JSON.stringify(process.env)
+      envVars = envVars.replace(',', '<br/>')
+      res.send('API works.<h1>DNS</h1>' + JSON.stringify(address) + '</br>' + JSON.stringify(family) + '<h1>ENV</h1>' + envVars)
     }
   })
 })
