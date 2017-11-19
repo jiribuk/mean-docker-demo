afcdef13782dd80cf91a6c0b530c89e420c2d70f
jiri <george.buk@gmail.com>
2017-11-18 19:28:56 -0500

    Formatting env vars

--- routes/api.js
+++ routes/api.js
@@ -12,13 +12,18 @@ const ArticleModel = require('../models/article')
 router.get('/', (req, res) => {
   const dns = require('dns')
 
+  let start = Date.now()
   dns.lookup('cos-service-nodeport-service', (err, address, family) => {
     if (err) {
       res.send(err).status(500)
     } else {
-      let envVars = JSON.stringify(process.env)
-      envVars = envVars.replace(',', '<br/>')
-      res.send('API works.<h1>DNS</h1>' + JSON.stringify(address) + '</br>' + JSON.stringify(family) + '<h1>ENV</h1>' + envVars)
+      let dnsTime = Date.now() - start
+      let envVarText = JSON.stringify(process.env)
+      envVarText = envVarText.replace(/\"/g, '')
+      let envVars = envVarText.split(',')
+      envVars.sort()
+      envVarText = envVars.join('<br/>')
+      res.send('API works.<h1>DNS</h1>' + JSON.stringify(address) + ' ' + dnsTime + 'ms' + '</br>' + JSON.stringify(family) + '<h1>ENV</h1>' + envVarText)
     }
   })
 })
